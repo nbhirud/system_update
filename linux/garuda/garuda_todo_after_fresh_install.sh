ab8e533205e3b8e81cf814984d94714a275109dc
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2024-09-24 16:59:42 -0400

    garuda - update command

--- linux/garuda/garuda_todo_after_fresh_install.sh
+++ linux/garuda/garuda_todo_after_fresh_install.sh
@@ -1,7 +1,18 @@
 
 
 # https://wiki.garudalinux.org/en/garuda-update
-garuda-update # update all packages
+garuda-update  # update all packages
+
+
+
+#################################################################
+# Custom linux aliases - add to ~/.zshrc
+#################################################################
+
+alias nbupdate="garuda-update && sudo freshclam && omz update"
+
+#################################################################
+
 
 
 
