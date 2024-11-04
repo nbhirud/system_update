1d0c771b883d4427a8909fd67a18203d46dd563c
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2024-11-03 21:07:02 -0500

    alpine - setup instructions as desktop OS

---
+++ linux/alpine/alpine_todo_after_fresh_install.sh
@@ -0,0 +1,12 @@
+
+#######################################################
+
+# Very detailed official setup instructions: https://wiki.alpinelinux.org/wiki/Installation
+
+# Install alpine as a desktop OS: https://wiki.alpinelinux.org/wiki/Installation#setup-alpine_based_System_Disk_Install
+setup-alpine # Command to initiate installation to disk
+# choose "sys"
+
+
+
+
