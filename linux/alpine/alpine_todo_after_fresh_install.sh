bb0b60724e8318d1bd32a0c28c6e4931db717154
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2024-11-03 21:10:41 -0500

    alpine - user creation

--- linux/alpine/alpine_todo_after_fresh_install.sh
+++ linux/alpine/alpine_todo_after_fresh_install.sh
@@ -7,6 +7,13 @@
 setup-alpine # Command to initiate installation to disk
 # choose "sys"
 
+#######################################################
+
+# Create user
+setup-user # uses adduser in the backend
+adduser -g "Nikhil Bhirud" nbhirud -s /bin/zsh -S
+adduser nbhirud wheel # add an existing user as admin user
+apk add doas # doas is a simplified and lightweight replacement for sudo
 
 
 
