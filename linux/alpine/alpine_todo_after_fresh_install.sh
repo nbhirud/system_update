ef54da65d5a8e5eb5580e3dece45566b664209a4
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2024-11-05 18:11:27 -0500

    alpine - repositories for updates, etc

--- linux/alpine/alpine_todo_after_fresh_install.sh
+++ linux/alpine/alpine_todo_after_fresh_install.sh
@@ -15,5 +15,12 @@ adduser -g "Nikhil Bhirud" nbhirud -s /bin/zsh -S
 adduser nbhirud wheel # add an existing user as admin user
 apk add doas # doas is a simplified and lightweight replacement for sudo
 
+# Repositories (from where you receive updates, etc)
+setup-apkrepos -cf
+# Check if this supports https: https://mirrors.alpinelinux.org
+# For better security you should probably change the url's from http to https in /etc/apk/repositories manually if supported, else change mirror/repo
+
+
+
 
 
