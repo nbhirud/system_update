183c5886ef06a42f575a16d28f1273ac57dd8a9f
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-02-13 18:14:47 -0500

    COMMON - set gnome default fonts using dconf

---
+++ linux/common/nerd_fonts.sh
@@ -0,0 +1,7 @@
+
+
+
+dconf write /org/gnome/desktop/interface/font-name "'Ubuntu Nerd Font 11'"
+dconf write /org/gnome/desktop/interface/document-font-name "'Ubuntu Nerd Font 11'"
+dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Ubuntu Nerd Font Bold 11'"
+dconf write /org/gnome/desktop/interface/monospace-font-name "'JetBrainsMono Nerd Font 10'"
