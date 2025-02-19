66876ba7685b65d7d0ef45f3c68bc02295b5b98c
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-02-19 09:45:03 -0500

    Fonts - note added about editing using dconf

--- linux/common/nerd_fonts.sh
+++ linux/common/nerd_fonts.sh
@@ -5,3 +5,5 @@ dconf write /org/gnome/desktop/interface/font-name "'Ubuntu Nerd Font 11'"
 dconf write /org/gnome/desktop/interface/document-font-name "'Ubuntu Nerd Font 11'"
 dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Ubuntu Nerd Font Bold 11'"
 dconf write /org/gnome/desktop/interface/monospace-font-name "'JetBrainsMono Nerd Font 10'"
+
+# You can also use the gsettings commandline tool instead of the Dconf Editor GUI tool
