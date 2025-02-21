afd101dd54d424b24265ff999894ae0b2810b77f
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-02-21 12:05:59 -0500

    Clamav - created scrpt for installing packages

---
+++ linux/security_os_level/clamav.sh
@@ -0,0 +1,29 @@
+packagesNeeded=(clamav clamd clamav-update clamtk)
+if [ -x "$(command -v apk)" ];
+then
+    # Alpine Linux
+    sudo apk add --no-cache "${packagesNeeded[@]}"
+
+elif [ -x "$(command -v pacman)" ];
+then
+    # Arch, etc.
+    sudo pacman -S "${packagesNeeded[@]}"
+
+elif [ -x "$(command -v apt-get)" ];
+then
+    # Debian, Ubuntu, etc.
+    sudo apt-get install "${packagesNeeded[@]}"
+
+elif [ -x "$(command -v dnf)" ];
+then
+    # Fedora, RHEL, etc.
+    sudo dnf install "${packagesNeeded[@]}"
+
+elif [ -x "$(command -v zypper)" ];
+then
+    # OpenSUSE, etc.
+    sudo zypper install "${packagesNeeded[@]}"
+
+else
+    echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: "${packagesNeeded[@]}"">&2;
+fi
