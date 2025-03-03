39216b9600927f472cd4f88a64b831bebf9e24d8
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-03-03 11:41:15 -0500

    Fonts - note on searching an installed font

--- linux/common/nerd_fonts.sh
+++ linux/common/nerd_fonts.sh
@@ -7,3 +7,6 @@ dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Ubuntu Nerd Font B
 dconf write /org/gnome/desktop/interface/monospace-font-name "'JetBrainsMono Nerd Font 10'"
 
 # You can also use the gsettings commandline tool instead of the Dconf Editor GUI tool
+
+# Example font search for setting as default as monospace font for firefox
+# fc-list | grep Nerd | grep -v Propo | grep -v Bold | grep -v Condense | grep -v Extra | grep -v Light | grep -v Italic | grep -v Thin | grep -v Medium | grep -v Black | grep "Noto" 
