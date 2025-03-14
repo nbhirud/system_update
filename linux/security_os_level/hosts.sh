adc7781b9a690556173ab1f01eabb0ed47dc0e8b
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-03-14 15:03:22 -0400

    hosts - set correct permission for new file

--- linux/security_os_level/hosts.sh
+++ linux/security_os_level/hosts.sh
@@ -9,17 +9,30 @@ mkdir -p hostsplay
 cd hostsplay
 echo "Created dir: $PWD"
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
+
+PERM=$(stat --format '%a' /etc/hosts)
+echo $PERM
+
 sudo mv /etc/hosts ./hosts_orig_$(date +%Y%m%d_%H%M%S)
 # modify the downloaded hosts file if necessary
 sudo mv hosts /etc/hosts
+
+# Set correct file permission for new hosts file
+# stat --format '%a' /etc/hosts  # for linux
+# stat -c '%a' <file>  # for busybox
+sudo chmod $PERM /etc/hosts
+
 # cd ..
 # sudo rm -r hostsplay
 
 ####################################
 
-sudo dnf -y install podman
+# sudo dnf -y install podman
+# https://github.com/StevenBlack/hosts/raw/refs/heads/master/Dockerfile
 
-https://github.com/StevenBlack/hosts/raw/refs/heads/master/Dockerfile
+####################################
+
+
 
 
 
