eeded4e1f5ecf75c37fbee906c244137852236b1
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-03-16 05:29:51 -0400

    Fedora - correction and default yes and echoing steps now

--- linux/fedora/run_first.sh
+++ linux/fedora/run_first.sh
@@ -3,6 +3,7 @@
 
 #######################################
 
+echo "************************ Updating /etc/dnf/dnf.conf ************************"
 # Configurations to dnf
 # https://dnf.readthedocs.io/en/latest/conf_ref.html
 # sudo nano /etc/dnf/dnf.conf
@@ -19,6 +20,7 @@ echo 'defaultyes=True' | sudo tee -a /etc/dnf/dnf.conf
 # LibreWolf, Mullvad browser and alacritty
 #######################################
 
+echo "************************ Adding Librewolf repo ************************"
 # LibreWolf - https://librewolf.net/installation/fedora/
 # add the repo
 
@@ -27,30 +29,38 @@ echo 'defaultyes=True' | sudo tee -a /etc/dnf/dnf.conf
 # cd nb/temp
 # wget https://librewolf.net/installation/fedora/
 # cat index.html | grep pkexec
+
+# add the repo
 curl -fsSL https://repo.librewolf.net/librewolf.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
 
+
+echo "************************ Adding Mullvad repo ************************"
 # https://mullvad.net/en/download/browser/linux
 # Add the Mullvad repository server to dnf
 # curl https://mullvad.net/en/download/browser/linux | grep addrepo
 sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
 
 
+echo "************************ Adding VSCodium repo ************************"
 ### VSCodium
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 
+echo "************************ Installing packages ************************"
 # install the package
 sudo dnf install -y librewolf alacritty git mullvad-browser codium
 
 #######################################
 
+echo "************************ Removing packages ************************"
 # remove stuff
 sudo dnf remove -y  totem yelp gnome-tour gnome-connections firefox
 # remove the gnome terminal ptyxis as we have installed 
 
 #######################################
 
+echo "************************ Enabling RPM Fusion ************************"
 # Enable RPM Fusion
 # https://rpmfusion.org/Configuration
 
@@ -58,33 +68,40 @@ sudo dnf remove -y  totem yelp gnome-tour gnome-connections firefox
 
 ### Configuration of Repositories - https://rpmfusion.org/Configuration
 
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm # use bash, not zsh (done above)
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm # use bash, not zsh (done above)
 
+echo "************************ Configuring @core ************************"
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
-sudo dnf update @core
+sudo dnf update -y @core
 
+echo "************************ Configuring Multimedia on Fedora ************************"
 ### Multimedia on Fedora - https://rpmfusion.org/Howto/Multimedia 
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
+sudo dnf update -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
+echo "************************ Configuring intel driver ************************"
 # Which intel driver to install -  https://wiki.archlinux.org/title/Hardware_video_acceleration
-sudo dnf install libva-intel-driver
+sudo dnf install -y libva-intel-driver
 
+echo "************************ Configuring Codecs ************************"
 ### Codecs - https://docs.fedoraproject.org/en-US/quick-docs/installing-plugins-for-playing-movies-and-music/
-sudo dnf group install multimedia
+sudo dnf group install -y multimedia
 
 #######################################
 
+echo "************************ Enabling flatpak flathub ************************"
 ### Enable flatpak flathub
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #######################################
 
+echo "************************ Changing default downloads dir ************************"
 # Change default downloads dir:
 mkdir -p /home/nbhirud/nb/Downloads
 xdg-user-dirs-update --set DOWNLOAD "/home/nbhirud/nb/Downloads"
 
 #######################################
 
+echo "************************ Update and upgrade everything ************************"
 sudo dnf update -y && sudo dnf upgrade --refresh -y
 
