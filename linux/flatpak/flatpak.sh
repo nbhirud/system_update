9c88e9a6842641bc753c262a0e478f6164ccbc08
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-03-26 13:12:48 -0400

    flatpak - FOSS messenger added, commented out some unused stuff

--- linux/flatpak/flatpak.sh
+++ linux/flatpak/flatpak.sh
@@ -10,23 +10,23 @@ flatpak update # Updates every outdated Flatpak package
 
 
 # Firefox:
-sudo flatpak install flathub org.mozilla.firefox
+# sudo flatpak install flathub org.mozilla.firefox
 # flatpak run org.mozilla.firefox
 
 # VSCodium
-sudo flatpak install flathub com.vscodium.codium # install it
+# sudo flatpak install flathub com.vscodium.codium # install it
 # flatpak run com.vscodium.codium # run it
 # sudo flatpak uninstall com.vscodium.codium # uninstall it
 
 # Pycharm
-sudo flatpak install -y flathub com.jetbrains.PyCharm-Community
+# sudo flatpak install -y flathub com.jetbrains.PyCharm-Community
 # flatpak run com.jetbrains.PyCharm-Community
 
 # Tor Browser
-sudo flatpak install tor-browser
+# sudo flatpak install tor-browser
 
 # Spotify
-flatpak install -y flathub com.spotify.Client
+# flatpak install -y flathub com.spotify.Client
 
 # Signal Desktop
 flatpak install flathub org.signal.Signal
@@ -38,7 +38,10 @@ flatpak install flathub org.signal.Signal
 # flatpak run com.usebottles.bottles
 
 # PlayOnLinux
-flatpak install -y flathub com.playonlinux.PlayOnLinux4
+# flatpak install -y flathub com.playonlinux.PlayOnLinux4
 
 # Manage flatpak permissions and file storage outside of the flatpak
-flatpak install flathub com.github.tchx84.Flatseal
+# flatpak install flathub com.github.tchx84.Flatseal
+
+# Facebook Messenger app
+flatpak install flathub com.sindresorhus.Caprine
