6ea7f1b3604fdb44ff805f2b4072d4b445188055
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-04-23 12:53:40 -0400

    Fedora - invoke the nerd fonts script from fedora setup script

--- linux/fedora/run_first.sh
+++ linux/fedora/run_first.sh
@@ -136,6 +136,9 @@ sudo dnf update -y && sudo dnf upgrade --refresh -y
 echo "************************ Update hosts file ************************"
 sudo sh linux/security_os_level/hosts.sh
 
+echo "************************ Setup nerd fonts ************************"
+sh linux/common/fonts.sh
+
 #######################################
 
 echo "************************ Install and configure more packages ************************"
