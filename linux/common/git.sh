a0a13060283889195b4ce9414e7bf03a20c38a18
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-05-01 14:31:49 -0400

    git - script to automate git setup

--- linux/common/git.sh
+++ linux/common/git.sh
@@ -1,8 +1,10 @@
 # Script to setup git on a new machine or new installation of OS
 
 # https://git-scm.com/book/en/v2/Customizing-Git-Git-Configuration
-$ git config --global user.name "Nikhil Bhirud"
-$ git config --global user.email "<place xyz@users.noreply.github.com from github here>"
+git config --global user.name "Nikhil Bhirud"
+
+# TODO replace with correct email
+# git config --global user.email "<place xyz@users.noreply.github.com from github here>"
 
 # https://code.visualstudio.com/docs/sourcecontrol/overview#_vs-code-as-git-difftool-and-mergetool
 git config --global core.editor "codium --wait" # # https://git-scm.com/book/en/v2/Appendix-C:-Git-Commands-Setup-and-Config
@@ -27,21 +29,21 @@ git config --global color.ui true # default is true
 # color.status
 
 ############################
-git config --global commit.template ~/.gitmessage.txt
-# TODO create the temopate file ~/.gitmessage.txt as follows (uncomment):
-# Subject line (try to keep under 50 characters)
+# git config --global commit.template ~/.gitmessage.txt
+# # TODO create the temopate file ~/.gitmessage.txt as follows (uncomment):
+# # Subject line (try to keep under 50 characters)
 
-# Multi-line description of commit,
-# feel free to be detailed.
+# # Multi-line description of commit,
+# # feel free to be detailed.
 
-# [Ticket: X]
+# # [Ticket: X]
 ############################
 
-git config --global core.excludesfile ~/.gitignore_global
-# create file ~/.gitignore_global with following content syntax (uncomment):
-# *~
-# .*.swp
-# .DS_Store
+# git config --global core.excludesfile ~/.gitignore_global
+# # create file ~/.gitignore_global with following content syntax (uncomment):
+# # *~
+# # .*.swp
+# # .DS_Store
 
 ############################
 
@@ -49,20 +51,20 @@ git config --global core.excludesfile ~/.gitignore_global
 # https://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks
 ############################
 
-# garbage collect
-# https://git-scm.com/book/en/v2/Git-Internals-Maintenance-and-Data-Recovery
-git gc 
-# git gc --auto
+# # garbage collect
+# # https://git-scm.com/book/en/v2/Git-Internals-Maintenance-and-Data-Recovery
+# git gc 
+# # git gc --auto
 
 ############################
-# git config --list
+# # git config --list
 
-# https://docs.github.com/en/get-started/git-basics/set-up-git
-gh auth login
+# # https://docs.github.com/en/get-started/git-basics/set-up-git
+# gh auth login
 
-# also see:
-# https://www.jetbrains.com/help/idea/using-git-integration.html
-# https://git-scm.com/book/en/v2/Appendix-A:-Git-in-Other-Environments-Git-in-IntelliJ-/-PyCharm-/-WebStorm-/-PhpStorm-/-RubyMine
-# https://git-scm.com/book/en/v2/Appendix-A:-Git-in-Other-Environments-Git-in-Zsh # if not using omz
+# # also see:
+# # https://www.jetbrains.com/help/idea/using-git-integration.html
+# # https://git-scm.com/book/en/v2/Appendix-A:-Git-in-Other-Environments-Git-in-IntelliJ-/-PyCharm-/-WebStorm-/-PhpStorm-/-RubyMine
+# # https://git-scm.com/book/en/v2/Appendix-A:-Git-in-Other-Environments-Git-in-Zsh # if not using omz
 
-# 
+# # 
