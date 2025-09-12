40f1cbe97fe3ac4fad01dfcec0105ad5013e6da4
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2025-09-12 11:57:38 -0400

    DNS - First attempt at automating DNS setup - does not work on a Gnome boxes VM

--- linux/security_os_level/dns.sh
+++ linux/security_os_level/dns.sh
@@ -1,156 +1,227 @@
+#!/usr/bin/env bash
 
-# Requirements
-# I tried nextdns and set it up on my android, linux, router, etc; basically everywhere. Then realized that the free plan has limitations on number of requests that I will reach quickly. So I need a Foss/free alternative to nextdns. Hosting on cloud platforms is not an option.
-# I should be able to:
-# - Set up this DNS IPs on my router
-# - Set up this DNS for DOH/DOT on my router
-# - Set it up on my linux desktops and laptops in WiFi/Ethernet config, and as a system wide DOH/DOT
-# - Set it up on my firefox and chrome browsers on linux
-# - set it up on android phones as private dns
-# - set it up on chrome browser on linux
-# - should be usable while not in my home WiFi too
-# - Use blocklists like StevenBlack hosts, and also manually block domains or tlds
+if [ "$EUID" -ne 0 ]; then
+  echo "[-] Please run as root (or with sudo). You are running as $(whoami)"
+  exit 1
+fi
 
-#########################################################
-##### Testing - DNS over HTTPS/TLS
-#########################################################
+set -eux
 
-resolvectl status # You can see the configured DNS
+NEXTDNS_ID="<YOUR_NEXTDNS_ID>"   # Replace with your NextDNS ID
+NEXTDNS_DEVICE_ID="<YOUR_NEXTDNS_DEVICE_ID>" # Replace with your NextDNS device ID
+CACHE_SIZE=5000                  # Aggressive cache
+TTL_MIN=3600                     # Minimum TTL (1hr)
+TTL_MAX=86400                    # Maximum TTL (1day)
+TTL_LOCAL=3600                   # Local TTL (1hr) - Cache hosts from /etc/hosts and other sources
+TTL_NEG=3600                     # Negative TTL (1hr)
+BLOCKLIST_URL="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts"
+BLOCKLIST_PATH_1="/etc/dnsmasq.d/hosts.blocklist"
 
-dig fedoraproject.org # Check "SERVER" towards the bottom should indicate that the query was answered by the DNS service at 127.0.0.53 or similar
+echo "[+] Installing required packages..."
+sudo dnf install -y dnsmasq wget
 
-nslookup fedoraproject.org
-# Would show something like:
-# Server:		<b>127.0.0.53</b>
-# Address:	<b>127.0.0.53#53</b>
+# echo "[+] Stopping services..."
+# sudo systemctl stop dnsmasq
+# sudo systemctl stop NetworkManager
+# sudo systemctl stop systemd-resolved
 
-host -v fedoraproject.org
-# The last line of output woule be like:
-# Received 115 bytes from 127.0.0.53#53 in 65 ms  
+echo "[+] Configuring dnsmasq..."
+sudo tee /etc/dnsmasq.conf > /dev/null <<EOF
+# dnsmasq configuration 
+# For details about options, go to: https://dnsmasq.org/docs/dnsmasq-man.html
+listen-address=127.0.0.1
+port=53
+# interface=lo
+# bind only to the above address
+bind-interfaces 
+# Do not use system resolver settings from /etc/resolv.conf
+no-resolv 
+# Use systemd-resolved as the upstream DNS server
+server=127.0.0.53 
+
+# Avoid conflicts
+# Enable code to detect DNS forwarding loops; ie the situation where a query sent to one of the upstream server eventually returns as a new query to the dnsmasq instance.
+dns-loop-detect
+# Don't poll /etc/resolv.conf for changes. 
+no-poll
+
+# Aggressive caching
+cache-size=${CACHE_SIZE}
+min-cache-ttl=${TTL_MIN}
+max-cache-ttl=${TTL_MAX}
+local-ttl=${TTL_LOCAL}
+neg-ttl=${TTL_NEG}
+
+# Speed optimizations
+# Allow more parallel queries
+dns-forward-max=150  
+
+# Logging
+log-facility=/var/log/dnsmasq.log
+# Optional for debugging - disable after initial setup. Disable later for privacy
+log-queries
+log-debug
+dumpfile=/var/log/dnsmasq_dump.log
 
 
-#########################################################
-##### NextDNS DNS over HTTPS/TLS
-#########################################################
-
-# Go to https://my.nextdns.io - and follow the Setup tab
-
-# Be sure to read "Identify your devices" section at the bottom of Setup page before starting
-
-# There would be details for linux, android , router, etc setups
-# Including:
-# DNS-over-TLS
-# DNS-over-HTTPS
-# IPv6
-# IPv4
-# DNS Servers
-
-# Setup Router, PC/Laptop resolved.conf, PC/Laptop Wired/Wireless connections, firefox browser, chrome browser,
+# Blocklist
+# Additional hosts file. Read the specified file as well as /etc/hosts.If a directory is given, then read all the files contained in that directory in alphabetical order. 
+# Specify a directory - TODO
+addn-hosts=${BLOCKLIST_PATH_1}
+# addn-hosts=${BLOCKLIST_PATH_2}
+# addn-hosts=${BLOCKLIST_PATH_3}
+# addn-hosts=${BLOCKLIST_PATH_4}
+# addn-hosts=${BLOCKLIST_PATH_5}
+# addn-hosts=${BLOCKLIST_PATH_6}
 
 
+# Security
+# Don't forward short names
+# domain-needed 
+# block RFC1918 queries from the internet (Block reverse lookups for private IPs)
+bogus-priv 
+# Remove any MAC address information already in downstream queries before forwarding upstream. 
+strip-mac
+# Remove any subnet address already present in a downstream query before forwarding it upstream.
+strip-subnet
+#Dnsmasq can scramble the case of letters in DNS queries it sends upstream as a security feature.
+do-0x20-encode
+# Prevent DNS-rebinding attacks
+stop-dns-rebind 
+# rebind-protection
 
-#########################################################
-##### Mullvad DNS over HTTPS
-#########################################################
+# Add DNSSEC validation (optional but recommended for security if not using DOH/DOT)
+# Validate DNS replies and cache DNSSEC data
+dnssec
+# trust-anchor=.,19036,8,2,49aac11d7b6f6446702e542a5a17ab467475a8e0101b5dc00119e701148f325d
+# even though systemd-resolved is not performing DNSSEC validation upstream, the local Dnsmasq instance is still securing  cached responses, which helps with both performance and integrity.
 
-# DNS over HTTPS
-# https://mullvad.net/en/help/dns-over-https-and-dns-over-tls#how-to-use
+# Disable things not being used:
+no-dhcp-interface
 
-sudo systemctl enable systemd-resolved
+EOF
 
+echo "[+] Downloading StevenBlack blocklist..."
+sudo mkdir -p /etc/dnsmasq.d/
+sudo wget -qO ${BLOCKLIST_PATH} ${BLOCKLIST_URL}
 
-# Open the Settings app and go to Network. Click on the settings icon for your connected network. On the IPv4 and IPv6 tabs, turn off Automatic using the radio button next to DNS, and leave the DNS field blank, then click on Apply.  Disable and enable the network using the on/off button to make sure it takes effect.
+echo "[+] Setting up cron for weekly blocklist updates..."
+sudo tee /etc/cron.weekly/update-dnsmasq-blocklist > /dev/null <<'EOF'
+#!/bin/sh
+wget -qO /etc/dnsmasq.d/hosts.blocklist https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+systemctl restart dnsmasq
+EOF
+sudo chmod +x /etc/cron.weekly/update-dnsmasq-blocklist
 
-# on fedora
-sudo nano /usr/lib/systemd/resolved.conf 
+echo "test dnsmasq config"
+dnsmasq --test
+sleep 5s
 
-# Add the following lines in the bottom under [Resolve]. Remove one # from the beginning of a line with the DNS option that you want to use:
+echo "[+] Configuring systemd-resolved for NextDNS DoT..."
+sudo tee /etc/systemd/resolved.conf > /dev/null <<EOF
+[Resolve]
+DNS=45.90.28.0#nbBoxes-5efc56.dns.nextdns.io
+DNS=2a07:a8c0::#nbBoxes-5efc56.dns.nextdns.io
+DNS=45.90.30.0#nbBoxes-5efc56.dns.nextdns.io
+DNS=2a07:a8c1::#nbBoxes-5efc56.dns.nextdns.io
+FallbackDNS=9.9.9.9
+FallbackDNS=2620:fe::fe
+FallbackDNS=149.112.112.112
+FallbackDNS=2620:fe::9
+DNSOverTLS=yes
+Domains=~.
+#DNSSEC=no
+Cache=no
+# ReadEtcHosts=yes
+# Disables local network discovery protocols to enhance privacy and security (prevents leaks)
+LLMNR=no
+MulticastDNS=no
+# Prevent systemd-resolved from binding to 127.0.0.53:53
+DNSStubListener=no
+EOF
 
-# #DNS=194.242.2.2 #dns.mullvad.net
-# #DNS=194.242.2.3 #adblock.dns.mullvad.net
-# #DNS=194.242.2.4 #base.dns.mullvad.net
-# #DNS=194.242.2.5 #extended.dns.mullvad.net
-# DNS=194.242.2.6 #family.dns.mullvad.net
-# #DNS=194.242.2.9 #all.dns.mullvad.net
-# DNSSEC=no
-# DNSOverTLS=yes
-# Domains=~.
+# Simplified the below by moving it to main file above
+# echo "[+] Disabling LLMNR & multicast DNS..."
+# sudo mkdir -p /etc/systemd/resolved.conf.d
+# sudo tee /etc/systemd/resolved.conf.d/disable-llmnr.conf > /dev/null <<EOF
+# [Resolve]
+# # Disables local network discovery protocols to enhance privacy and security (prevents leaks)
+# LLMNR=no
+# MulticastDNS=no
+# # Prevent systemd-resolved from binding to 127.0.0.53:53
+# DNSStubListener=no
+# EOF
 
+echo "[+] Configuring NetworkManager to use dnsmasq (and not override DNS)..."
+sudo mkdir -p /etc/NetworkManager/conf.d
+sudo tee /etc/NetworkManager/conf.d/dns.conf > /dev/null <<EOF
+[main]
+dns=none
+EOF
+
+echo "[+] Enabling services..."
+sudo systemctl enable --now systemd-resolved
+sudo systemctl enable --now dnsmasq
+
+echo "[+] Updating /etc/resolv.conf to use dnsmasq..."
+sudo rm -f /etc/resolv.conf
 sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+sudo sed -i 's/^nameserver .*/nameserver 127.0.0.1/' /run/systemd/resolve/stub-resolv.conf
 
+# To prevent overwrites, make it immutable (optional but recommended):
+# sudo chattr +i /etc/resolv.conf
+
+echo "[+] Restarting services..."
+sudo systemctl daemon-reload # Reload systemd
+sudo systemctl restart dnsmasq
+sudo systemctl restart NetworkManager
 sudo systemctl restart systemd-resolved
 
-sudo systemctl restart NetworkManager
 
+# echo "[+] Hardening firewall: Blocking external DNS (UDP/53)..."
+# # Block plain DNS except localhost
+# sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" destination port=53 protocol="udp" drop'
+# sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" destination port=53 protocol="tcp" drop'
+# sudo firewall-cmd --reload
+
+echo "[+] Setup complete!"
+echo "Test with dig"
+dig fedoraproject.org @127.0.0.1
+sleep 5s
+
+echo "Test with dig"
+dig fedoraproject.org
+sleep 5s
+
+echo "Check NextDNS status:"
+curl -s https://test.nextdns.io
+sleep 5s
+
+echo "The output should show only nameserver 127.0.0.1."
+cat /etc/resolv.conf
+sleep 5s
+
+echo "test the ad-blocking"
+dig doubleclick.net
+sleep 5s
+
+echo "Verify systemd-resolved configuration"
 resolvectl status
+sleep 5s
 
-# In case it doesn't work, change to this setting in/etc/systemd/resolved.conf:
-# DNSOverTLS=opportunistic
+echo "This should not be very useful at this point, but still"
+resolvectl statistics
+sleep 5s
 
-#########################################################
-##### From Gnome common
-#########################################################
+echo "check dnsmasq.service - journalctl"
+journalctl -xeu dnsmasq.service
+sleep 5s
 
-# Network -> Click the gear box next to the connection in use -> IPV4 tab -> Disable "Automatic" for
-# DNS -> Paste the following in the box and apply:
-# 194.242.2.6, 194.242.2.4, 1.1.1.3, 1.0.0.3, 9.9.9.9, 149.112.112.112, 1.1.1.2, 1.0.0.2
+echo "check dnsmasq.service - systemctl"
+systemctl status dnsmasq.service
+sleep 5s
 
-
-# 194.242.2.6 - Mullvad DNS - block Ads, Trackers, Malware, Adult, Gambling (https://family.dns.mullvad.net/dns-query)
-# 194.242.2.4 - Mullvad DNS - block Ads, Trackers, Malware (https://base.dns.mullvad.net/dns-query)
-# 1.1.1.3, 1.0.0.3 - Cloudflare DNS - Block malware and adult content (https://family.cloudflare-dns.com/dns-query)
-# 1.1.1.2, 1.0.0.2 - Cloudflare DNS - Block malware (https://security.cloudflare-dns.com/dns-query)
-# 9.9.9.9, 149.112.112.112 -quad9 DNS - Malware Blocking, DNSSEC Validation (https://dns.quad9.net/dns-query)
-# Rethink DNS - create DNS over HTTPS url from here - https://rethinkdns.com/configure
-
-# Network -> Click the gear box next to the connection in use -> IPV6 tab -> "Additional DNS servers" -> Paste
-# the following in the box and apply:
-# 2606:4700:4700::1112, 2606:4700:4700::1002, 2606:4700:4700::1111, 2606:4700:4700::1001, 2001:4860:4860::8888, 2001:4860:4860::8844
-
-# Useful links abot DNS:
-# https://mullvad.net/en/help/dns-over-https-and-dns-over-tls#how-to-use
-# https://developers.cloudflare.com/1.1.1.1/ip-addresses/
-# https://pkg.cloudflare.com/index.html
-# https://developers.cloudflare.com/1.1.1.1/encryption/dns-over-https/dns-over-https-client/
-# https://developers.cloudflare.com/1.1.1.1/setup/linux/
-# https://developers.cloudflare.com/1.1.1.1/setup/router/
-# https://developers.cloudflare.com/1.1.1.1/other-ways-to-use-1.1.1.1/dns-over-tor/
-# https://blog.cloudflare.com/welcome-hidden-resolver/
-
-
-
-
-########################
-
-## HTTPS (DoH) Servers in this order:
-## Setup on Router and/or PC
-# https://security.cloudflare-dns.com/dns-query
-# https://dns.quad9.net/dns-query
-# https://cloudflare-dns.com/dns-query
-
-########################
-
-## Time servers
-# time.cloudflare.com
-# time.nist.gov
-# time-nw.nist.gov
-# pool.ntp.org
-
-########################
-
-#########################################################
-##### alpine
-#########################################################
-
-# https://wiki.alpinelinux.org/wiki/Configure_Networking
-
-#########################################################
-##### 
-#########################################################
-
-
-#########################################################
-##### 
-#########################################################
-
+echo "Confirm caching works:"
+# tail -f /var/log/dnsmasq.log
+tail -100 /var/log/dnsmasq.log | cat
 
