f0f3c9d316de372f9832295a29434b8c185902f6
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2026-01-30 11:55:25 -0500

    KDE_Connect - Script to install GSConnect requirements on a Gnome DE system

---
+++ linux/common/kde_connect.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eux
+
+
+#### GSConnect = KDE Connect on Gnome
+
+# Help - https://github.com/GSConnect/gnome-shell-extension-gsconnect/wiki/Help
+# https://github.com/GSConnect/gnome-shell-extension-gsconnect/wiki/Error#openssl-not-found
+
+sudo dnf install -y openssl nautilus-python nautilus-extensions nautilus-gsconnect webextension-gsconnect gnome-shell-extension-gsconnect 
+
+# Install GSConnect extension via extension manager
+
+dconf write /org/gnome/shell/disable-user-extensions false
+
+# gapplication launch org.gnome.Shell.Extensions.GSConnect # if error, run next line
+systemctl --user reload dbus-broker.service
