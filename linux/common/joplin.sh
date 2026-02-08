bedbaf461e541a6645c175c7bc2339446ca15064
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2026-02-08 11:43:51 -0500

    Joplin - Server setup WIP

---
+++ linux/common/joplin.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eux
+
+# https://github.com/laurent22/joplin/blob/dev/packages/server/README.md
+
+# Install Docker and Docker Compose before this step
+# Verify Installations
+docker --version
+docker-compose --version
+
+# Set Up Joplin Server with Docker Compose
+joplin-server
+
+HOME_DIR=$(getent passwd $USER | cut -d: -f6)
+CODE_BASE_DIR="$HOME_DIR/nb/CodeProjects"
+SYSUPDATE_CODE_DIR="$CODE_BASE_DIR/system_update"
+DEST_DIR="$HOME_DIR/.local/share/fonts/nerd-fonts"
+
+
+mkdir -p ~/joplin-server && cd ~/joplin-server
