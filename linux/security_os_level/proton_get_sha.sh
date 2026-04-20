0d6bb3c430cd6b8b5e57281e81b2dbb9f9394673
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2026-04-20 17:19:30 -0400

    ProtonAG - Changes to the parsing and fetching of SHA from versions.json. Still WIP

--- linux/security_os_level/proton_get_sha.sh
+++ linux/security_os_level/proton_get_sha.sh
@@ -2,56 +2,57 @@
 
 # set -eux
 
-# JSON_PATH="/home/nbhirud/nb/Downloads/proton_ag_downloads_2025-09-22_13-01-59"
-# JSON_FILENAME="ProtonAuthenticator_version.json"
-
 JSON_PATH=$1
 JSON_FILENAME=$2
+
+# JSON_PATH="/home/nbhirud/nb/Downloads/ProtonAG/proton_ag_downloads_2026-03-09_11-05-41"
+# JSON_FILENAME="ProtonPass_version.json"
 
 # jq '.Releases[0].File' $JSON_PATH/$JSON_FILENAME | jq length
 
 # latest_file=$(jq '.Releases[0].File' $JSON_PATH/$JSON_FILENAME)
 # echo $latest_file
 
-exit # TODO remove after completing the below new code
+distro_count=$(jq '.Releases[0].File' "$JSON_PATH"/"$JSON_FILENAME" | jq length)
 
-release_count=$(jq '.Releases' "$JSON_PATH"/"$JSON_FILENAME" | jq length)
-# echo $release_count
-
-i_stable=0
-
-# i=0
-# while [ $i -lt "$release_count" ]
-# do
-#     # URL=$(jq ".Releases[0].File[$i].Url" "$JSON_PATH"/"$JSON_FILENAME")
-#     CategoryName=$(jq ".Releases[$i].CategoryName" "$JSON_PATH"/"$JSON_FILENAME")
-#     if echo "$CategoryName" | grep -q "Stable" ;
-#     then
-#         i_stable=$i
-#     fi
-#     i=$(( $i + 1 ))
-# done
-
-distro_count=$(jq '.Releases[i_stable].File' "$JSON_PATH"/"$JSON_FILENAME" | jq length)
+################### new ###################################
+##exit # TODO remove after completing the below new code
+#
+#i_stable=0
+#distro_count=$(jq '.Releases[${i_stable}].File' "$JSON_PATH"/"$JSON_FILENAME" | jq length)
+#release_count=$(jq '.Releases' "$JSON_PATH"/"$JSON_FILENAME" | jq length)
+#
+## echo $release_count
+#
+## i=0
+## while [ $i -lt "$release_count" ]
+## do
+##     # URL=$(jq ".Releases[0].File[$i].Url" "$JSON_PATH"/"$JSON_FILENAME")
+##     CategoryName=$(jq ".Releases[$i].CategoryName" "$JSON_PATH"/"$JSON_FILENAME")
+##     if echo "$CategoryName" | grep -q "Stable" ;
+##     then
+##         i_stable=$i
+##     fi
+##     i=$(( $i + 1 ))
+## done
+#
+#distro_count=$(jq '.Releases[i_stable].File' "$JSON_PATH"/"$JSON_FILENAME" | jq length)
+##################### /new ################################
 # echo $distro_count
 
 i=0
-while [ $i -lt "$distro_count" ]
-do
-    URL=$(jq ".Releases[i_stable].File[$i].Url" "$JSON_PATH"/"$JSON_FILENAME")
+while [ $i -lt "$distro_count" ]; do
+  URL=$(jq ".Releases[0].File[$i].Url" "$JSON_PATH"/"$JSON_FILENAME")
 
-    # if grep -q foo file.txt; then
-    #     echo "file.txt contains foo"
+  # if grep -q foo file.txt; then
+  #     echo "file.txt contains foo"
 
-    if echo "$URL" | grep -q ".rpm" ;
-    then
-        # echo $i
-        # echo "$URL found it"
-        Sha512CheckSum=$(jq ".Releases[i_stable].File[$i].Sha512CheckSum" "$JSON_PATH"/"$JSON_FILENAME")
-        echo "$Sha512CheckSum"
-    fi
+  if echo "$URL" | grep -q ".rpm"; then
+    # echo $i
+    # echo "$URL found it"
+    Sha512CheckSum=$(jq ".Releases[0].File[$i].Sha512CheckSum" "$JSON_PATH"/"$JSON_FILENAME")
+    echo "$Sha512CheckSum"
+  fi
 
-    i=$(( $i + 1 ))
+  i=$(($i + 1))
 done
-
-
