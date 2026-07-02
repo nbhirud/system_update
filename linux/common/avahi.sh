368ccc35c57af5a2bd606f675103c0ff0898d60e
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2026-07-02 14:26:57 -0400

    avahi - added firewall rule

--- linux/common/avahi.sh
+++ linux/common/avahi.sh
@@ -2,8 +2,19 @@
 echo "************************ Setup avahi mDNS ************************"
 sudo dnf install avahi
 
-sudo systemctl start avahi-daemon
-sudo systemctl enable avahi-daemon
+# sudo systemctl start avahi-daemon
+# sudo systemctl enable avahi-daemon
+sudo systemctl enable --now avahi-daemon
+
+# Firewall
+sudo firewall-cmd --permanent --add-service=mdns
+sudo firewall-cmd --reload
+
+sleep 5s # Check mDNS *.local in status
+
+##################################################
+# Debugging
+##################################################
 
 # sudo systemctl status avahi-daemon 
 sleep 5s # Check mDNS *.local in status
