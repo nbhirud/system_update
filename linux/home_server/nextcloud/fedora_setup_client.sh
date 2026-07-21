fee026bdd8e4e7fef25140686003b3573ed88002
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2026-07-21 16:03:50 -0400

    HomeServer - Code for nextcloud

---
+++ linux/home_server/nextcloud/fedora_setup_client.sh
@@ -0,0 +1,6 @@
+
+
+
+
+# On the client (not on server)
+# sudo dnf install -y nextcloud-client-nautilus # intrgrate with Gnome file manager
---
+++ linux/home_server/nextcloud/fedora_setup_server.sh
@@ -0,0 +1,15 @@
+
+
+
+
+sudo docker run \
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart always \
+--publish 80:80 \
+--publish 8080:8080 \
+--publish 8443:8443 \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+--env NEXTCLOUD_DATADIR="/home/nbhirud/nb/nextcloud_data" \
+ghcr.io/nextcloud-releases/all-in-one:latest
